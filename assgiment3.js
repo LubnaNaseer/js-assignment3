ff39ea99ee0260b495d8b3ca37289f6bd7c5c421
LUBNANASEER <lubeenaseer@gmail.com>
2025-06-28 12:43:03 +0500

    Add files via upload

---
+++ assgiment3.js
@@ -0,0 +1,19 @@
+// Q1;
+let age = 20;
+alert("I am " + age + "year old");
+// Q2;
+ let visit = "you have vistied this 14 times";
+ alert(visit);
+
+//  Q3;
+let brithyear = 2005;
+document.writeln("My birth year is" + brithyear + "<br><br>"+ "Data type of my variable is number <br> <br><br>" );
+
+// Q4;
+let  visitername ="Johan Doe"
+let product ="T- shirt(s)";
+let quantity = 5;
+ document.writeln(visitername  + " " + "orderd" + " " + quantity + " " + product + " " + "on XYZ Clothing");
+ 
+
+
